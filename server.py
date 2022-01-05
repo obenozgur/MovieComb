5176c469c3145ad61cb872e7ed8ed1c64ea07eb9
Oben Ozgur <ozguro18@itu.edu.tr>
2022-01-05 18:32:46 +0300

    Icon added for website.

--- server.py
+++ server.py
@@ -63,7 +63,8 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     port = app.config.get("PORT", 5000)
     app.run(host="0.0.0.0", port=port)
